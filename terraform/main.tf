71c346366f732947354d4ffbe8ca31035f882dc2
Tasneem Hegazy <tasneem.hegazy@acutebusiness.com>
2025-05-07 23:44:27 +0300

    add Azure Kubernetes Service resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,20 @@
      name     = "devops-demo-rg"
      location = "germanywestcentral"
    }
+
+   resource "azurerm_kubernetes_cluster" "main" {
+     name                = "devops-demo-aks"
+     location            = azurerm_resource_group.main.location
+     resource_group_name = azurerm_resource_group.main.name
+     dns_prefix          = "devopsdemo"
+
+     default_node_pool {
+       name       = "default"
+       node_count = 2
+       vm_size    = "Standard_B2s"
+     }
+
+     identity {
+       type = "SystemAssigned"
+     }
+   }
