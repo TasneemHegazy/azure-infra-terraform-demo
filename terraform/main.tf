723681f4536156834c926cfcc16745bcc83bd201
Tasneem Hegazy <tasneem.hegazy@acutebusiness.com>
2025-05-27 13:12:59 +0300

    fix Indentations

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,42 @@
-   terraform {
-     required_providers {
-       azurerm = {
-         source  = "hashicorp/azurerm"
-         version = "~> 3.0"
-       }
-     }
-     required_version = ">= 1.0.0"
-   }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
 
-   provider "azurerm" {
-     features {}
-   }
+provider "azurerm" {
+  features {}
+}
 
-   resource "azurerm_resource_group" "main" {
-     name     = var.resource_group_name
-     location = var.location
-   }
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
-   resource "azurerm_kubernetes_cluster" "main" {
-     name                = "devops-demo-aks"
-     location            = azurerm_resource_group.main.location
-     resource_group_name = azurerm_resource_group.main.name
-     dns_prefix          = "devopsdemo"
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "devops-demo-aks"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "devopsdemo"
 
-     sku_tier = "Free"
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
 
-     default_node_pool {
-       name       = "default"
-       node_count = 1
-       vm_size    = "Standard_B2s"
-     }
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
-     identity {
-       type = "SystemAssigned"
-     }
-   }
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+}
